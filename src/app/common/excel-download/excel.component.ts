1b6305f960b5816e2d6afb045c1274fad891b82b
jinha <jinha4ever@gmail.com>
2021-04-29 11:30:36 +0900

    공통 엑셀다운로드 파일명 동적으로 수정

--- src/app/common/excel-download/excel.component.ts
+++ src/app/common/excel-download/excel.component.ts
@@ -14,6 +14,7 @@ import { UserExcelService } from './userExcel.service';
 export class ExcelComponent implements OnInit {
   
   users: User[];
+ 
 
   constructor(private router : Router, 
               private userService : UserExcelService, 
@@ -27,8 +28,9 @@ export class ExcelComponent implements OnInit {
     });
   };
 
-  exportAsXLSX():void {
-    this.excelService.exportAsExcelFile(this.users, 'ExcelFile');
+  exportAsXLSX(listTitle:string):void {
+   
+    this.excelService.exportAsExcelFile(this.users, listTitle);
   }
   
 
