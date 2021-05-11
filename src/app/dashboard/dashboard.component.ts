6315aae5b64b075654050f1948891bbd26cacb3c
홍송희 <songhee.hong@myvtw.co.kr>
2021-05-11 16:05:46 +0900

    팀별 오늘의 일정 개수 확인

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { Schedule } from '../models/schedule.model';
+import { Team } from '../models/team.model';
 import { User } from '../models/user.model';
 import { JwtService } from '../services/jwt.service';
 import { ScheduleService } from '../services/schedule.service';
+import { TeamService } from '../services/team.service';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -17,11 +20,15 @@ export class DashboardComponent implements OnInit {
 
   userList : User[];
   scheduleList : Schedule[];
+  teamList : Team[];
+
+  teamControl;
 
   constructor(
     private jwtService : JwtService,
     private userService : UserService,
-    private scheduleService : ScheduleService
+    private scheduleService : ScheduleService,
+    private teamService : TeamService
   ) { }
 
   ngOnInit(): void {
@@ -40,14 +47,35 @@ export class DashboardComponent implements OnInit {
     }
 
     this.userService.selectUserList().subscribe(res => {//같은 팀 유저 리스트
-      this.userList = res.data.filter((user) => user.team == this.loginUser.team && user.id != this.loginUser.id);
+      this.userList = res.data.filter((user) => user.team == this.loginUser.team);
     });
 
     this.scheduleService.selectScheduleList().subscribe(res => {
       this.scheduleList = res.data.filter((user) => user.team == this.loginUser.team);
 
       //스케쥴 개수 설정
-      this.loginUser.count = this.scheduleList.filter((schedule) => schedule.writer == this.loginUser.id).length;
+      for (let i = 0; i < this.userList.length; i++) {
+        const user = this.userList[i];
+        user.count = this.scheduleList.filter((schedule) => schedule.writer == user.id).length;
+      }
+    });
+
+    this.teamService.selectTeamList().subscribe(res => {
+      this.teamList = res.data.team;
+    });
+
+    this.teamControl = new FormControl(this.loginUser.team);
+  }
+
+  changeTeam(){
+    this.userService.selectUserList().subscribe(res => {//선택한 팀 유저 리스트
+      this.userList = res.data.filter((user) => user.team == this.teamControl.value);
+    });
+
+    this.scheduleService.selectScheduleList().subscribe(res => {
+      this.scheduleList = res.data.filter((user) => user.team == this.teamControl.value);
+
+      //스케쥴 개수 설정
       for (let i = 0; i < this.userList.length; i++) {
         const user = this.userList[i];
         user.count = this.scheduleList.filter((schedule) => schedule.writer == user.id).length;
