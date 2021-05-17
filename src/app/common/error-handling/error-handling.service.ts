7efe68c60ce55430fe8bb08a8809b174ac28c7be
jinha <jinha4ever@gmail.com>
2021-05-17 13:18:43 +0900

    에러 수정

--- src/app/common/error-handling/error-handling.service.ts
+++ src/app/common/error-handling/error-handling.service.ts
@@ -6,15 +6,16 @@ import { ErrorHandler } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class ErrorHandlingService implements ErrorHandler {
+export class ErrorHandlingService {
+//implements ErrorHandler {
 
   private userUrl = '/error';
 
   constructor(private http:HttpClient) { }
 
-  handleError(error: any): void {
-    throw new Error('Method not implemented.');
-  }
+//  handleError(error: any): void {
+//    throw new Error('Method not implemented.');
+//  }
 
   
 }
